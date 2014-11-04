ffdf1a137608fcaf4c1bedf118401e47221b3ae8
Glenn Tweedie <glenn.tweedie@digitaldialogue.com.au>
2014-11-04 16:36:40 +0800

    shift initializer to World from Tabletop
    currently Tabletop isn't doing anything. Maybe remove it?

--- lib/tabletop.rb
+++ lib/tabletop.rb
@@ -1,8 +1,4 @@
 module Bot
   class Tabletop < World
-    def initialize(width=1, height=1)
-      @height = height
-      @width  = width
-    end
   end
 end
--- lib/world.rb
+++ lib/world.rb
@@ -2,6 +2,11 @@ module Bot
   class World
     attr_reader :height, :width
 
+    def initialize(width=1, height=1)
+      @height = height
+      @width  = width
+    end
+
     def valid_x?(x)
       (0..@width).cover? x
     end
